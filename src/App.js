cf23d0f2b77aaf85aa62d3a4b7ff845cc7d1ecc6
hanan320 <Hanoona3-20@hotmail.com>
2024-05-20 08:48:42 +0000

    some edite in form css file and do navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import Footer from './componant/footer/Footer';
 import Form from './componant/form/Form';
+import Navbar from './componant/navbar/Navbar';
+
 
 
 
 function App() {
   return (
-    <> <div class="content">
-      <Form />
-      <Footer />
-    </div>
+    <>
+
+      <div class="content">
+        <Navbar />
+        <Form />
+        <Footer />
+      </div>
 
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
